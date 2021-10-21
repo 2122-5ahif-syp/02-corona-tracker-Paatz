1825651684ddc41e4da35c62dbfc6484b7a1315b
Paatz <patrick.spisak@gmx.at>
2021-10-21 14:06:47 +0200

    Repository added, and Postgres DB runs

--- backend/src/main/java/at/htl/control/PersonRepository.java
+++ backend/src/main/java/at/htl/control/PersonRepository.java
@@ -1,9 +1,19 @@
 package at.htl.control;
 
+import at.htl.entities.Person;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.transaction.Transactional;
 
 @Transactional
 @ApplicationScoped
-public class PersonRepository {
+public class PersonRepository implements PanacheRepository<Person> {
+
+    @Transactional
+    public Person save(Person personToSave) {
+        return getEntityManager().merge(personToSave);
+    }
+
+
 }
