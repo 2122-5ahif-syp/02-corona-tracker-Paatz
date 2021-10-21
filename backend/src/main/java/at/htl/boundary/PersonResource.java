a44d44b04662ceb17755e186acb2de8b9d1638c6
Paatz <patrick.spisak@gmx.at>
2021-10-21 14:22:21 +0200

    added POST-Endpoit

--- backend/src/main/java/at/htl/boundary/PersonResource.java
+++ backend/src/main/java/at/htl/boundary/PersonResource.java
@@ -1,12 +1,17 @@
 package at.htl.boundary;
 
 import at.htl.control.PersonRepository;
+import at.htl.entities.Person;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+import java.sql.Time;
+import java.sql.Timestamp;
 
 @RequestScoped
 @Path("/persons")
@@ -14,8 +19,23 @@ public class PersonResource {
     @Inject
     PersonRepository personRepository;
 
-    @GET
+    /*@GET
     public Response findAll(){
         return Response.ok(personRepository.findAllPersons()).build();
+    }*/
+
+    @POST
+    public Response createPerson(
+        @FormParam("firstName") String firstName,
+        @FormParam("lastName") String lastName,
+        @FormParam("email") String email,
+        @FormParam("time") Timestamp time
+    ){
+        Person person = new Person();
+        person.setFirstName(firstName);
+        person.setLastName(lastName);
+        person.setEmail(email);
+        person.setTime( new Timestamp(System.currentTimeMillis()));
+        return Response.status(200).entity(person).build();
     }
 }
