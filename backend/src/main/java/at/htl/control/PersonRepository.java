d69029459b259bd1d4b5e1b2e6a539ea4e158e82
Paatz <patrick.spisak@gmx.at>
2021-10-21 14:05:23 +0200

    Entity added

---
+++ backend/src/main/java/at/htl/control/PersonRepository.java
@@ -0,0 +1,9 @@
+package at.htl.control;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
+
+@Transactional
+@ApplicationScoped
+public class PersonRepository {
+}
